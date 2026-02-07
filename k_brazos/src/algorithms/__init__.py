147c5ebf85446033f0e7957fbdb7ef2a9b721a15
Gloria Casals <gloria.casals97@gmail.com>
2026-02-07 17:28:03 +0100

    adjustment to access new modules

--- k_brazos/src/algorithms/__init__.py
+++ k_brazos/src/algorithms/__init__.py
@@ -15,8 +15,10 @@ For more details about GPL-3.0: https://www.gnu.org/licenses/gpl-3.0.html
 # Importación de módulos o clases
 from .algorithm import Algorithm
 from .epsilon_greedy import EpsilonGreedy
+from .epsilon_decay import EpsilonDecay
 from .ucb import UCB1
+from .ucb2 import UCB2
 from .softmax import Softmax
 
 # Lista de módulos o clases públicas
-__all__ = ['Algorithm', 'EpsilonGreedy', 'UCB1', 'Softmax']
+__all__ = ['Algorithm', 'EpsilonGreedy', 'EpsilonDecay', 'UCB1', 'UCB2', 'Softmax']
