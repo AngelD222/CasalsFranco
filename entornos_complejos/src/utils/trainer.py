008dfc21954f382d87b0d200e340f07ce0628b00
AngelD222 <angelfranco2000@gmail.com>
2026-02-19 20:46:39 +0100

    Update trainer.py

--- entornos_complejos/src/utils/trainer.py
+++ entornos_complejos/src/utils/trainer.py
@@ -1,7 +1,7 @@
 # Vamos a guardar el dato de la función f(t) = len(episodio_t)
 
 from tqdm import tqdm
-from src.policies import get_epsilon_decay
+from src.policies import epsilon_decay
 
 def train_agent(env, agent, n_episodes, initial_eps=1.0, final_eps=0.01, decay_rate=0.005):
     """
@@ -10,7 +10,7 @@ def train_agent(env, agent, n_episodes, initial_eps=1.0, final_eps=0.01, decay_r
     """
     for episode in tqdm(range(n_episodes), desc="Entrenando agente"):
         # 1. Actualizamos el valor de epsilon para este episodio
-        current_epsilon = get_epsilon_decay(episode, initial_eps, final_eps, decay_rate)
+        current_epsilon = epsilon_decay(episode, initial_eps, final_eps, decay_rate)
         # Inyectamos el epsilon actualizado al agente (ya que nuestra clase base lo usará)
         agent.epsilon = current_epsilon 
         
