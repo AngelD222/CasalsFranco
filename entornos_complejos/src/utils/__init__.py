5fdb095ccf877f7ce1e455773ca324ccca33092a
AngelD222 <angelfranco2000@gmail.com>
2026-02-19 18:20:31 +0100

    Update __init__.py

--- entornos_complejos/src/utils/__init__.py
+++ entornos_complejos/src/utils/__init__.py
@@ -1,9 +1,12 @@
 from .trainer import train_agent
 from .seeder import set_seed, make_env
+from .plotter import plot_episode_rewards, plot_episode_lengths
 
 # Lista de funciones públicas accesibles al importar desde src.utils
 __all__ = [
     'train_agent',
     'set_seed',
-    'make_env'
+    'make_env',
+    'plot_episode_rewards',
+    'plot_episode_lengths'
 ]
