b1954c2e9c1b5daa6b1e8bed310805e32dc093ce
AngelD222 <angelfranco2000@gmail.com>
2026-02-26 12:35:32 +0100

    Update __init__.py

--- entornos_complejos/src/agents/__init__.py
+++ entornos_complejos/src/agents/__init__.py
@@ -2,8 +2,15 @@
 from .clase_BASE import Agent
 from .monte_carlo import AgentMonteCarloTodasVisitas
 
+from monte_carlo_off import AgentMonteCarloOffPolicy
+from q_learning import AgentQLearning
+from sarsa import AgentSARSA
+
 # Lista de clases públicas accesibles al importar desde src.agents
 __all__ = [
     'Agent',
-    'AgentMonteCarloTodasVisitas'
+    'AgentMonteCarloTodasVisitas',
+    'AgentMonteCarloOffPolicy',
+    'AgentQLearning',
+    'AgentSARSA'
 ]
