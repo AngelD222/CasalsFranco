18979fff860145bd62e3667cb581665904eb1f6d
AngelD222 <angelfranco2000@gmail.com>
2026-02-27 10:43:12 +0100

    Update __init__.py

--- entornos_complejos/src/utils/__init__.py
+++ entornos_complejos/src/utils/__init__.py
@@ -1,6 +1,6 @@
 from .trainer import train_agent
 from .seeder import set_seed, make_env
-from .plotter import plot_episode_rewards, plot_episode_lengths, plot_win_rate, plot_multiple_seeds_rewards, plot_value_estimation_error, plot_multiple_seeds_metrics
+from .plotter import plot_episode_rewards, plot_episode_lengths, plot_win_rate, plot_multiple_seeds_rewards, plot_value_estimation_error, plot_multiple_seeds_rewards2
 from .replay_buffer import ReplayBuffer
 
 # Lista de funciones públicas accesibles al importar desde src.utils
@@ -14,5 +14,5 @@ __all__ = [
     'plot_multiple_seeds_rewards',
     'ReplayBuffer'
     'plot_value_estimation_error',
-    'plot_multiple_seeds_metrics'
+    'plot_multiple_seeds_rewards2'
 ]
